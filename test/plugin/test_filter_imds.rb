705eecfd1df7567c37778e0d38d955e18fc52161
Matthew Juel <matthewjuel@Matthews-MacBook-Pro.local>
2020-04-15 09:36:11 -0700

    formatted IMDS

--- test/plugin/test_filter_imds.rb
+++ test/plugin/test_filter_imds.rb
@@ -12,6 +12,27 @@ class ImdsFilterTest < Test::Unit::TestCase
   CONFIG = %[
     containerIdInput "\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000a0a000a0-0000-0a00-aaa0-aaaa00aa0a00\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000NextKVPKey\u0000\u0000\u0000\u0000\u0000NextKVPValue\u0000\u0000\u0000\u0000\u0000"
   ]
+  IMDS = <<-TEXT
+    {\"compute\":{\"azEnvironment\":\"AzurePublicCloud\",\"customData\":\"\",\"location\":\"eastus\",
+    \"name\":\"fluentd-test2\",\"offer\":\"UbuntuServer\",\"osType\":\"Linux\",\"placementGroupId\":\"\",
+    \"plan\":{\"name\":\"\",\"product\":\"\",\"publisher\":\"\"},\"platformFaultDomain\":\"0\",\"platformUpdateDomain\":\"0\",
+    \"provider\":\"Microsoft.Compute\",\"publicKeys\":[],\"publisher\":\"Canonical\",
+    \"resourceGroupName\":\"juelm-imds-fluentd\",
+    \"resourceId\":\"/subscriptions/0000a0a0-0a0a-000a-0000-000a000aa0a/resourceGroups/juelm-imds-fluentd/providers/Microsoft.Compute/virtualMachines/fluentd-test2\",
+    \"sku\":\"18.04-LTS\",\"storageProfile\":{\"dataDisks\":[],\"imageReference\":{\"id\":\"\",\"offer\":\"UbuntuServer\",
+    \"publisher\":\"Canonical\",\"sku\":\"18.04-LTS\",\"version\":\"latest\"},\"osDisk\":{\"caching\":\"ReadWrite\",
+    \"createOption\":\"FromImage\",\"diffDiskSettings\":{\"option\":\"\"},\"diskSizeGB\":\"30\",
+    \"encryptionSettings\":{\"enabled\":\"false\"},\"image\":{\"uri\":\"\"},
+    \"managedDisk\":{\"id\":\"/subscriptions/0000a0a0-0a0a-000a-0000-000a000aa0a/resourceGroups/JUELM-IMDS-FLUENTD/providers/Microsoft.Compute/disks/fluentd-test2_disk1_b2a49f76712c41aa850453e182f6c4e1\",
+    \"storageAccountType\":\"Premium_LRS\"},\"name\":\"fluentd-test2_disk1_b2a49f76712c41aa850453e182f6c4e1\",
+    \"osType\":\"Linux\",\"vhd\":{\"uri\":\"\"},\"writeAcceleratorEnabled\":\"false\"}},
+    \"subscriptionId\":\"0000a0a0-0a0a-000a-0000-000a000aa0a\",\"tags\":\"\",\"tagsList\":[],
+    \"version\":\"18.04.202003170\",\"vmId\":\"a7ff7831-57cf-4fa6-9016-726d1c81dfdf\",\"vmScaleSetName\":\"\",
+    \"vmSize\":\"Standard_B2s\",\"zone\":\"\"},
+    \"network\":{\"interface\":[{\"ipv4\":{\"ipAddress\":[{\"privateIpAddress\":\"172.16.0.5\",
+    \"publicIpAddress\":\"52.179.11.145\"}],\"subnet\":[{\"address\":\"172.16.0.0\",\"prefix\":\"24\"}]},
+    \"ipv6\":{\"ipAddress\":[]},\"macAddress\":\"000D3A12811D\"}]}}
+  TEXT
 
   def create_driver(conf = CONFIG)
     Fluent::Test::Driver::Filter.new(Fluent::Plugin::ImdsFilter).configure(conf)
@@ -27,7 +48,7 @@ class ImdsFilterTest < Test::Unit::TestCase
   	  'Metadata'=>'true',
   	  'User-Agent'=>'Ruby'
       }).
-    to_return(status: 200, body: "{\"compute\":{\"subscriptionId\":\"0000a0a0-0a0a-000a-0000-000a000aa0a\", \"location\":\"eastus\", \"resourceGroupName\":\"test-resource-group\", \"name\":\"test-vm\", \"vmSize\":\"Standard_B2s\", \"vmId\":\"a0aa0000-00aa-0aa0-0000-000a0a00aaaa\", \"placementGroupId\":\"\"}}", headers: {})
+    to_return(status: 200, body: IMDS, headers: {})
     d = create_driver()
     d.run do
       d.feed("test1", @time, {"Matt says" => "Hello"})
@@ -35,10 +56,10 @@ class ImdsFilterTest < Test::Unit::TestCase
     assert_equal(d.filtered_records[0]["Matt says"], "Hello")
     assert_equal(d.filtered_records[0]["subscriptionId"], "0000a0a0-0a0a-000a-0000-000a000aa0a")
     assert_equal(d.filtered_records[0]["region"], "eastus")
-    assert_equal(d.filtered_records[0]["resourceGroup"], "test-resource-group")
-    assert_equal(d.filtered_records[0]["vmName"], "test-vm")
+    assert_equal(d.filtered_records[0]["resourceGroup"], "juelm-imds-fluentd")
+    assert_equal(d.filtered_records[0]["vmName"], "fluentd-test2")
     assert_equal(d.filtered_records[0]["vmSize"], "Standard_B2s")
-    assert_equal(d.filtered_records[0]["vmId"], "a0aa0000-00aa-0aa0-0000-000a0a00aaaa")
+    assert_equal(d.filtered_records[0]["vmId"], "a7ff7831-57cf-4fa6-9016-726d1c81dfdf")
     assert_equal(d.filtered_records[0]["placementGroup"], "") 
     assert_equal(d.filtered_records[0]["containerID"], "a0a000a0-0000-0a00-aaa0-aaaa00aa0a00")
   end
@@ -55,7 +76,7 @@ class ImdsFilterTest < Test::Unit::TestCase
   	  'Metadata'=>'true',
   	  'User-Agent'=>'Ruby'
       }).
-    to_return(status: 404, body: "{\"compute\":{\"subscriptionId\":\"0000a0a0-0a0a-000a-0000-000a000aa0a\", \"location\":\"eastus\", \"resourceGroupName\":\"test-resource-group\", \"name\":\"test-vm\", \"vmSize\":\"Standard_B2s\", \"vmId\":\"a0aa0000-00aa-0aa0-0000-000a0a00aaaa\", \"placementGroupId\":\"\"}}", headers: {})
+    to_return(status: 404, body: IMDS, headers: {})
     d = create_driver()
     d.run do
       d.feed("test1", @time, {"Matt says" => "Hello"})
